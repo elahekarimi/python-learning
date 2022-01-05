e263e017c76d287fd113d0f6074adf22561284eb
elahe karimi <elikarimi7019@gmail.com>
2022-01-05 11:57:14 +0330

    update create love calculator

--- Day2/love-calculature.py
+++ Day2/love-calculature.py
@@ -12,6 +12,12 @@ o=names_lowercase.count('o')
 v=names_lowercase.count('v')
 e=names_lowercase.count('e')
 love=l+o+v+e
-sum_all=str(trues) + str(love)
-print(sum_all,"%")
+sum_all=int(str(trues) + str(love))
+if (sum_all < 10)  or (sum_all > 90):
+    print(sum_all,"%",'you go together like coke and mentos')
+elif ( sum_all >= 40) and (sum_all <= 50):
+    print(sum_all,"%","you are alrigt together")
+else:
+    print(sum_all,'%')
+
 
