f1fd9a2d4b3e17261ec0677eb74e351b3e3fbde9
elahe karimi <elikarimi7019@gmail.com>
2022-01-21 20:57:09 +0330

    create calculator for price of painting home

--- Day8/caesar_cripher.py
+++ Day8/caesar_cripher.py
@@ -1,4 +1,12 @@
-def birds (name, number):
-    print(f"the bird's name is:{name}")
-    print(f"my favourit bird's is:{number}")
-birds("joojoo","5joojoo")
+
+a=input('enter the wight:')
+b=input('enter the hight:')
+a=int(a)
+b=int(b)
+def paint_calcu (wight, hight):
+    coverage=5
+    sqr=(wight * hight) / coverage
+    print(sqr)
+paint_calcu(a,b)
+
+
