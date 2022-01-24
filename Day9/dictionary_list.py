99c17d00b1148dcbc6226b7aebc795a3d43fa440
elahe karimi <elikarimi7019@gmail.com>
2022-01-24 13:01:15 +0330

    create some dictionary

---
+++ Day9/dictionary_list.py
@@ -0,0 +1,9 @@
+from numpy import empty
+
+
+name_dictionary={"eli":"karimi", "goli":"golbaba", "maman": "karimi"}
+print(name_dictionary["eli"])
+name_dictionary["baab"]="kelaye"
+print(name_dictionary["baab"])
+name_dictionary={}
+print(name_dictionary)
