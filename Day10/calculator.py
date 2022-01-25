48c2108e39dd72124f27b2dfff325a96e83efd93
elahe karimi <elikarimi7019@gmail.com>
2022-01-25 18:24:16 +0330

    create a calculator function

---
+++ Day10/calculator.py
@@ -0,0 +1,33 @@
+from ast import operator
+import re
+
+
+#add
+def add (n1, n2):
+    return n1+n2
+#subtract
+def sub (n1, n2):
+    return n1-n2
+#multiple
+def mul (n1, n2):
+    return n1*n2
+#divide
+def div (n1, n2):
+    return n1/n2
+print(add(2,3))
+operators={
+    "+":add,
+    "-":sub,
+    "*":mul,
+    "/":div,
+}
+n1=int((input("enter first number:")))
+n2=int((input("enter second nmber:")))
+for symbol in operators:
+    print(symbol)
+operation_symbol=input("what operation do you want:")
+calculation_function=operators[operation_symbol]
+answer=calculation_function(n1,n2)
+print(f"{n1} {operation_symbol} {n2} = {answer}")
+
+        
