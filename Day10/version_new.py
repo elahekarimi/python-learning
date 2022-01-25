cb0c462f50a231cb1ce4b5438aaea10ffdc1ebb4
elahe karimi <elikarimi7019@gmail.com>
2022-01-25 17:33:00 +0330

    practice for new funcion

--- Day10/version_new.py
+++ Day10/version_new.py
@@ -1,6 +1,8 @@
 def name (first, last):
+    if first == "" or last =="":
+        return "there is not valid input"
     a=first.title()
     b=last.title()
     return f"{a}{b}"
-print(name("eli", "karimi"))
+print(name(input("input your first name:"),input("input your last name:")))
 
