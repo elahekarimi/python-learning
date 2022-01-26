062f47fd78a9a747c16a9d72913f469310334d0b
elahe karimi <elikarimi7019@gmail.com>
2022-01-27 00:32:16 +0330

    practice scope

--- Day12/scope_role.py
+++ Day12/scope_role.py
@@ -1,8 +1,15 @@
-b=15
-def eli():
-    a=2
-    print(a)
-    def goli():
-        print(b)
-    goli()
-eli()
+# b=15
+# def eli():
+#     a=2
+#     print(a)
+#     def goli():
+#         print(b)
+#     goli()
+# eli()
+game_enemy=3
+def create_enemy():
+    enemys=["skeleton", "zombi", "alien"]
+    if game_enemy < 5:
+        new_enemy=enemys[0]
+    print(new_enemy)
+create_enemy()
