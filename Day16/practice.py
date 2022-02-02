c8b423bff117ce1bd7db120a4ceeb50a59dd5bd9
elahe karimi <elikarimi7019@gmail.com>
2022-02-02 11:21:31 +0330

    improve the practice

--- Day16/practice.py
+++ Day16/practice.py
@@ -1,5 +1,14 @@
-# import anothermojol
-# print(anothermojol.value)
+import anothermojol
+print(anothermojol.value)
 from turtle import Turtle
 name=Turtle()
 print(name)
+name.shape("turtle")
+name.color("blue")
+
+from turtle import Screen
+
+
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
