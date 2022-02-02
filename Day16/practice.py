1de6adddf5165f8192f382b2fd28ff664caba663
elahe karimi <elikarimi7019@gmail.com>
2022-02-02 13:26:42 +0330

    create table

--- Day16/practice.py
+++ Day16/practice.py
@@ -1,14 +1,23 @@
-import anothermojol
-print(anothermojol.value)
-from turtle import Turtle
-name=Turtle()
-print(name)
-name.shape("turtle")
-name.color("blue")
+# import anothermojol
+# print(anothermojol.value)
+# from turtle import Turtle
+# name=Turtle()
+# print(name)
+# name.shape("turtle")
+# name.color("blue")
+# name.shapesize(2,6)
+# name.forward(200)
 
-from turtle import Screen
+# from turtle import Screen
 
 
-my_screen=Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen=Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("pokemon name", ["pika cho", "squirrel", "chandler"])
+table.add_column("type", ["electric", "water", "fire"])
+print(table)
+
