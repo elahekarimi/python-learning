8f78ab7f67e3233c097751755a76fb8716a7cb2f
elahe karimi <elikarimi7019@gmail.com>
2022-02-04 22:32:34 +0330

    create table

---
+++ Day17/quize_brain.py
@@ -0,0 +1,8 @@
+class QuizeBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"{self.question_number}Q: {current_question.text} (true, false)")
