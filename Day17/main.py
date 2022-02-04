d2a44e10fef0c651865fb9eac897e356411f25e6
elahe karimi <elikarimi7019@gmail.com>
2022-02-04 22:35:24 +0330

    create table

--- Day17/main.py
+++ Day17/main.py
@@ -1,24 +1,13 @@
-class User:
-    def __init__(self, user_id, username, ):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
+from data import QUESTIONS_data
+from question_model import Question
+from quize_brain import QuizeBrain
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
+question_bank = []
+for questions in QUESTIONS_data:
+    questions_text = questions["text"]
+    questions_answer = questions["text"]
+    new_question = Question(questions_text, questions_answer)
+    question_bank.append(new_question)
 
-
-user_1 = User("123", "elahe")
-user_2 = User("124", "goli")
-user_1.follow(user_2)
-# print(user_1.username)
-# print(user_1.id)
-print(user_1.followers)
-print(user_1.following)
-
-# user_1.id = "123"
-# user_1.username = "angela"
-# print(user_1.username)
-# print(user_1.id)
+quiz = QuizeBrain(question_bank)
+quiz.next_question()
