832c0201d2d949fef86fe31cbc4a3a5747414988
elahe karimi <elikarimi7019@gmail.com>
2022-02-06 22:03:59 +0330

    create table

--- Day18/main.py
+++ Day18/main.py
@@ -1,7 +1,8 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+tim_the_turtle = Turtle()
+tim_the_turtle.shape("turtle")
+tim_the_turtle.color("red")
+for _ in range(4):
+    tim_the_turtle.forward(100)
+    tim_the_turtle.right(90)
