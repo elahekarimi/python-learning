459761d8db43a867442722a19cd45313d069274b
elahe karimi <elikarimi7019@gmail.com>
2022-02-06 23:30:03 +0330

    create table

--- Day18/main.py
+++ Day18/main.py
@@ -1,8 +1,17 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
 
-tim_the_turtle = Turtle()
-tim_the_turtle.shape("turtle")
-tim_the_turtle.color("red")
-for _ in range(4):
-    tim_the_turtle.forward(100)
-    tim_the_turtle.right(90)
+tim = t.Turtle()
+color = ["navy", "green yellow", "gold", "crimson", "orchid"]
+
+
+def each(angle_side):
+    for _ in range(angle_side):
+        angle = 360 / angle_side
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side in range(3, 10):
+    tim.color(random.choice(color))
+    each(shape_side)
