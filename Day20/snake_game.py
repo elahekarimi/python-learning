6cbc1ab2ed1b8b882119a05c921ae56b5441c32c
elahe karimi <elikarimi7019@gmail.com>
2022-02-10 00:55:50 +0330

    create snake game

--- Day20/snake_game.py
+++ Day20/snake_game.py
@@ -11,15 +11,18 @@ screen.setup(height=600, width=600)
 screen.title("snake game")
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
