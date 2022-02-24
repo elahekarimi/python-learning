067f65bde91a360a35298665c84b9fab7a506f67
elahe karimi <elikarimi7019@gmail.com>
2022-02-24 18:32:48 +0330

    add Error Handling to this code with create a function and put code on it

--- Day26/main26.py
+++ Day26/main26.py
@@ -1,6 +1,18 @@
 import pandas
+
 data = pandas.read_csv("nato_phonetic.csv")
 phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
-word = input("enter a word: ").upper()
-out_list = [phonetic_dic[letter] for letter in word]
-print(out_list)
+
+
+def generate_phonetic():
+    word = input("enter a word: ").upper()
+    try:
+        out_list = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print("just enter word")
+        generate_phonetic()
+    else:
+        print(out_list)
+
+
+generate_phonetic()
