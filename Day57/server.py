5a11657035057f7b61e562f1a3f7181decbb9be8
elahe karimi <elikarimi7019@gmail.com>
2022-05-05 11:28:11 +0430

    5. URL Building with Flask

--- Day57/server.py
+++ Day57/server.py
@@ -24,6 +24,13 @@ def guss(name):
     age_data = age_responce.json()
     age = age_data["age"]
     return render_template("guss.html", person_name=name, gender=gender, age=age)
+@app.route("/blog/<num>")
+def blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    blog_responce = requests.get(blog_url)
+    all_posts = blog_responce.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
